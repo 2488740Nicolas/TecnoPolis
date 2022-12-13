5cb1ba29c10805ff65e6ff9ee485eb1ced8b77cc
Nicolas Mosquera Henao <niko20578@gmail.com>
2022-12-12 21:23:26 -0500

    Primer commit

---
+++ products.js
@@ -0,0 +1,62 @@
+// Crear productos deportivos
+const products = [{
+        id: 1,
+        name: 'Nothing Phone',
+        price: 1479900,
+        img: 'img/Elect1.png'
+    },
+    {
+        id: 2,
+        name: 'Tablet',
+        price: 854910,
+        img: 'img/Elect2.png'
+    },
+    {
+        id: 3,
+        name: 'Laptop Gamer Acer Aspire',
+        price: 3079900,
+        img: 'img/Elect3.png'
+    },
+    {
+        id: 4,
+        name: 'Pc Computador Gamer',
+        price: 3859900,
+        img: 'img/Elect4.png'
+    },
+    {
+        id: 5,
+        name: 'PlayStation 5',
+        price: 3999900,
+        img: 'img/Elect5.png'
+    },
+    {
+        id: 6,
+        name: 'Impresora Creality 3D',
+        price: 2069900,
+        img: 'img/Elect6.png'
+    },
+    {
+        id: 7,
+        name: 'Nintendo Switch',
+        price: 1699900,
+        img: 'img/Elect7.png'
+    },
+    {
+        id: 8,
+        name: 'Audífonos in-ear F9-5',
+        price: 37400,
+        img: 'img/Elect8.png'
+    },
+    {
+        id: 9,
+        name: 'Kit de Teclado y Mouse Gamer',
+        price: 94900,
+        img: 'img/Elect9.png'
+    },
+    {
+        id: 10,
+        name: 'Silla Gamer Trust GXT 716 Rizza',
+        price: 1903900,
+        img: 'img/Elect10.png'
+    },
+];
